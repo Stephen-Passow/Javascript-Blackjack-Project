70d6bb1aa64ec8ba5b153235ed2aaa246b6342de
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-10-04 12:22:33 -0400

    started on shuffle/deck

--- script.js
+++ script.js
@@ -1,92 +1,62 @@
-// //card suits and values
-// let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-// let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-
-// const Deck = {
-//   constructor() {
-//     let deck = [];
-
-//     const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
-//     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
-
-//     for (let i=0; i < suits.length; i++) {
-//       for (let x=0; x < values.length; x++) {
-//         deck.push(`${values} of ${suits}`);
-//       }
-//     }
-//     return deck;
-//   }
-// }
-// console.log(Deck.constructor)
-
-// const Deck = {
-//     createDeck () {
-//       let deck = [];
-
-//     const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
-//     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
-
-//       for (let i=0; i < suits.length; i++) {
-//       for (let x=0; x < values.length; x++) {
-//         deck.push(`${values} of ${suits}`);
-//   }
-//     }
-//     console.log()
-//   }
-// }
 const deckArray = [
     {suit: 'hearts', value: 2 , numer: 2 , image: '2_of_hearts.png'},
     {suit: 'hearts', value: 3 , number: 3, image: '3_of_hearts.png'},
-    {card: 'hearts', value: 4 , number: 4, image: '4_of_hearts.png'},
-    {card: 'hearts', value: 5 , number: 5, image: '5_of_hearts.png'},
-    {card: 'hearts', value: 6 , number: 6, image: '6_of_hearts.png'},
-    {card: 'hearts', value: 7 , number: 7, image: '7_of_hearts.png'},
-    {card: 'hearts', value: 8 , number: 8, image: '8_of_hearts.png'},
-    {card: 'hearts', value: 9 , number: 9, image: '9_of_hearts.png'},
-    {card: 'hearts', value: 10 , number: J, image: 'J_of_hearts.png'},
-    {card: 'hearts', value: 10 , number: Q, image: 'Q_of_hearts.png'},
-    {card: 'hearts', value: 10 , number: K, image: 'K_of_hearts.png'},
-    {card: 'hearts', value: 11 , number: A, image: 'A_of_hearts.png'},
+    {suit: 'hearts', value: 4 , number: 4, image: '4_of_hearts.png'},
+    {suit: 'hearts', value: 5 , number: 5, image: '5_of_hearts.png'},
+    {suit: 'hearts', value: 6 , number: 6, image: '6_of_hearts.png'},
+    {suit: 'hearts', value: 7 , number: 7, image: '7_of_hearts.png'},
+    {suit: 'hearts', value: 8 , number: 8, image: '8_of_hearts.png'},
+    {suit: 'hearts', value: 9 , number: 9, image: '9_of_hearts.png'},
+    {suit: 'hearts', value: 10 , number: J, image: 'J_of_hearts.png'},
+    {suit: 'hearts', value: 10 , number: Q, image: 'Q_of_hearts.png'},
+    {suit: 'hearts', value: 10 , number: K, image: 'K_of_hearts.png'},
+    {suit: 'hearts', value: 11 , number: A, image: 'A_of_hearts.png'},
 
     {suit: 'spades', value: 2 , numer: 2 , image: '2_of_spades.png'},
     {suit: 'spades', value: 3 , number: 3, image: '3_of_spades.png'},
-    {card: 'spades', value: 4 , number: 4, image: '4_of_spades.png'},
-    {card: 'spades', value: 5 , number: 5, image: '5_of_spades.png'},
-    {card: 'spades', value: 6 , number: 6, image: '6_of_spades.png'},
-    {card: 'spades', value: 7 , number: 7, image: '7_of_spades.png'},
-    {card: 'spades', value: 8 , number: 8, image: '8_of_spades.png'},
-    {card: 'spades', value: 9 , number: 9, image: '9_of_spades.png'},
-    {card: 'spades', value: 10 , number: J, image: 'J_of_spades.png'},
-    {card: 'spades', value: 10 , number: Q, image: 'Q_of_spades.png'},
-    {card: 'spades', value: 10 , number: K, image: 'K_of_spades.png'},
-    {card: 'spades', value: 11 , number: A, image: 'A_of_spades.png'},
+    {suit: 'spades', value: 4 , number: 4, image: '4_of_spades.png'},
+    {suit: 'spades', value: 5 , number: 5, image: '5_of_spades.png'},
+    {suit: 'spades', value: 6 , number: 6, image: '6_of_spades.png'},
+    {suit: 'spades', value: 7 , number: 7, image: '7_of_spades.png'},
+    {suit: 'spades', value: 8 , number: 8, image: '8_of_spades.png'},
+    {suit: 'spades', value: 9 , number: 9, image: '9_of_spades.png'},
+    {suit: 'spades', value: 10 , number: J, image: 'J_of_spades.png'},
+    {suit: 'spades', value: 10 , number: Q, image: 'Q_of_spades.png'},
+    {suit: 'spades', value: 10 , number: K, image: 'K_of_spades.png'},
+    {suit: 'spades', value: 11 , number: A, image: 'A_of_spades.png'},
 
     {suit: 'clubs', value: 2 , numer: 2 , image: '2_of_clubs.png'},
     {suit: 'clubs', value: 3 , number: 3, image: '3_of_clubs.png'},
-    {card: 'clubs', value: 4 , number: 4, image: '4_of_clubs.png'},
-    {card: 'clubs', value: 5 , number: 5, image: '5_of_clubs.png'},
-    {card: 'clubs', value: 6 , number: 6, image: '6_of_clubs.png'},
-    {card: 'clubs', value: 7 , number: 7, image: '7_of_clubs.png'},
-    {card: 'clubs', value: 8 , number: 8, image: '8_of_clubs.png'},
-    {card: 'clubs', value: 9 , number: 9, image: '9_of_clubs.png'},
-    {card: 'clubs', value: 10 , number: J, image: 'J_of_clubs.png'},
-    {card: 'clubs', value: 10 , number: Q, image: 'Q_of_clubs.png'},
-    {card: 'clubs', value: 10 , number: K, image: 'K_of_clubs.png'},
-    {card: 'clubs', value: 11 , number: A, image: 'A_of_clubs.png'},
+    {suit: 'clubs', value: 4 , number: 4, image: '4_of_clubs.png'},
+    {suit: 'clubs', value: 5 , number: 5, image: '5_of_clubs.png'},
+    {suit: 'clubs', value: 6 , number: 6, image: '6_of_clubs.png'},
+    {suit: 'clubs', value: 7 , number: 7, image: '7_of_clubs.png'},
+    {suit: 'clubs', value: 8 , number: 8, image: '8_of_clubs.png'},
+    {suit: 'clubs', value: 9 , number: 9, image: '9_of_clubs.png'},
+    {suit: 'clubs', value: 10 , number: J, image: 'J_of_clubs.png'},
+    {suit: 'clubs', value: 10 , number: Q, image: 'Q_of_clubs.png'},
+    {suit: 'clubs', value: 10 , number: K, image: 'K_of_clubs.png'},
+    {suit: 'clubs', value: 11 , number: A, image: 'A_of_clubs.png'},
 
-    {suit: 'diamonds', value: 2 , numer: 2 , image: '2_of_spades.png'},
-    {suit: 'diamonds', value: 3 , number: 3, image: '3_of_spades.png'},
-    {card: 'diamonds', value: 4 , number: 4, image: '4_of_spades.png'},
-    {card: 'diamonds', value: 5 , number: 5, image: '5_of_spades.png'},
-    {card: 'diamonds', value: 6 , number: 6, image: '6_of_spades.png'},
-    {card: 'diamonds', value: 7 , number: 7, image: '7_of_spades.png'},
-    {card: 'diamonds', value: 8 , number: 8, image: '8_of_spades.png'},
-    {card: 'diamonds', value: 9 , number: 9, image: '9_of_spades.png'},
-    {card: 'diamonds', value: 10 , number: J, image: 'J_of_spades.png'},
-    {card: 'diamonds', value: 10 , number: Q, image: 'Q_of_spades.png'},
-    {card: 'diamonds', value: 10 , number: K, image: 'K_of_spades.png'},
-    {card: 'diamonds', value: 11 , number: A, image: 'A_of_spades.png'},
-
+    {suit: 'diamonds', value: 2 , numer: 2 , image: '2_of_diamonds.png'},
+    {suit: 'diamonds', value: 3 , number: 3, image: '3_of_diamonds.png'},
+    {suit: 'diamonds', value: 4 , number: 4, image: '4_of_diamonds.png'},
+    {suit: 'diamonds', value: 5 , number: 5, image: '5_of_diamonds.png'},
+    {suit: 'diamonds', value: 6 , number: 6, image: '6_of_diamonds.png'},
+    {suit: 'diamonds', value: 7 , number: 7, image: '7_of_diamonds.png'},
+    {suit: 'diamonds', value: 8 , number: 8, image: '8_of_diamonds.png'},
+    {suit: 'diamonds', value: 9 , number: 9, image: '9_of_diamonds.png'},
+    {suit: 'diamonds', value: 10 , number: J, image: 'J_of_diamonds.png'},
+    {suit: 'diamonds', value: 10 , number: Q, image: 'Q_of_diamonds.png'},
+    {suit: 'diamonds', value: 10 , number: K, image: 'K_of_diamonds.png'},
+    {suit: 'diamonds', value: 11 , number: A, image: 'A_of_diamonds.png'},
   ]
     
-  
+let deck = deckArray[Math.floor(Math.random()*deckArray.length)];
+console.log(deck)
+
+
+function shuffle() {
+for (i=0; i < 1000; i++);
+
+}
