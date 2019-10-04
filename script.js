a6a44555b22a5d17eb76dbb35c5aa4152c4edbd6
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-10-04 15:00:06 -0400

    added script to grab a random card in deck

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const deckArray = [
     {suit: 'hearts', value: 7 , number: 7, image: '7_of_hearts.png'},
     {suit: 'hearts', value: 8 , number: 8, image: '8_of_hearts.png'},
     {suit: 'hearts', value: 9 , number: 9, image: '9_of_hearts.png'},
-    {suit: 'hearts', value: 10 , number: J, image: 'J_of_hearts.png'},
-    {suit: 'hearts', value: 10 , number: Q, image: 'Q_of_hearts.png'},
-    {suit: 'hearts', value: 10 , number: K, image: 'K_of_hearts.png'},
-    {suit: 'hearts', value: 11 , number: A, image: 'A_of_hearts.png'},
+    {suit: 'hearts', value: 10 , number: 'J', image: 'J_of_hearts.png'},
+    {suit: 'hearts', value: 10 , number: 'Q', image: 'Q_of_hearts.png'},
+    {suit: 'hearts', value: 10 , number: 'K', image: 'K_of_hearts.png'},
+    {suit: 'hearts', value: 11 , number: 'A', image: 'A_of_hearts.png'},
 
     {suit: 'spades', value: 2 , numer: 2 , image: '2_of_spades.png'},
     {suit: 'spades', value: 3 , number: 3, image: '3_of_spades.png'},
@@ -20,10 +20,10 @@ const deckArray = [
     {suit: 'spades', value: 7 , number: 7, image: '7_of_spades.png'},
     {suit: 'spades', value: 8 , number: 8, image: '8_of_spades.png'},
     {suit: 'spades', value: 9 , number: 9, image: '9_of_spades.png'},
-    {suit: 'spades', value: 10 , number: J, image: 'J_of_spades.png'},
-    {suit: 'spades', value: 10 , number: Q, image: 'Q_of_spades.png'},
-    {suit: 'spades', value: 10 , number: K, image: 'K_of_spades.png'},
-    {suit: 'spades', value: 11 , number: A, image: 'A_of_spades.png'},
+    {suit: 'spades', value: 10 , number: 'J', image: 'J_of_spades.png'},
+    {suit: 'spades', value: 10 , number: 'Q', image: 'Q_of_spades.png'},
+    {suit: 'spades', value: 10 , number: 'K', image: 'K_of_spades.png'},
+    {suit: 'spades', value: 11 , number: 'A', image: 'A_of_spades.png'},
 
     {suit: 'clubs', value: 2 , numer: 2 , image: '2_of_clubs.png'},
     {suit: 'clubs', value: 3 , number: 3, image: '3_of_clubs.png'},
@@ -33,10 +33,10 @@ const deckArray = [
     {suit: 'clubs', value: 7 , number: 7, image: '7_of_clubs.png'},
     {suit: 'clubs', value: 8 , number: 8, image: '8_of_clubs.png'},
     {suit: 'clubs', value: 9 , number: 9, image: '9_of_clubs.png'},
-    {suit: 'clubs', value: 10 , number: J, image: 'J_of_clubs.png'},
-    {suit: 'clubs', value: 10 , number: Q, image: 'Q_of_clubs.png'},
-    {suit: 'clubs', value: 10 , number: K, image: 'K_of_clubs.png'},
-    {suit: 'clubs', value: 11 , number: A, image: 'A_of_clubs.png'},
+    {suit: 'clubs', value: 10 , number: 'J', image: 'J_of_clubs.png'},
+    {suit: 'clubs', value: 10 , number: 'Q', image: 'Q_of_clubs.png'},
+    {suit: 'clubs', value: 10 , number: 'K', image: 'K_of_clubs.png'},
+    {suit: 'clubs', value: 11 , number: 'A', image: 'A_of_clubs.png'},
 
     {suit: 'diamonds', value: 2 , numer: 2 , image: '2_of_diamonds.png'},
     {suit: 'diamonds', value: 3 , number: 3, image: '3_of_diamonds.png'},
@@ -46,17 +46,18 @@ const deckArray = [
     {suit: 'diamonds', value: 7 , number: 7, image: '7_of_diamonds.png'},
     {suit: 'diamonds', value: 8 , number: 8, image: '8_of_diamonds.png'},
     {suit: 'diamonds', value: 9 , number: 9, image: '9_of_diamonds.png'},
-    {suit: 'diamonds', value: 10 , number: J, image: 'J_of_diamonds.png'},
-    {suit: 'diamonds', value: 10 , number: Q, image: 'Q_of_diamonds.png'},
-    {suit: 'diamonds', value: 10 , number: K, image: 'K_of_diamonds.png'},
-    {suit: 'diamonds', value: 11 , number: A, image: 'A_of_diamonds.png'},
+    {suit: 'diamonds', value: 10 , number: 'J', image: 'J_of_diamonds.png'},
+    {suit: 'diamonds', value: 10 , number: 'Q', image: 'Q_of_diamonds.png'},
+    {suit: 'diamonds', value: 10 , number: 'K', image: 'K_of_diamonds.png'},
+    {suit: 'diamonds', value: 11 , number: 'A', image: 'A_of_diamonds.png'},
   ]
     
 let deck = deckArray[Math.floor(Math.random()*deckArray.length)];
 console.log(deck)
 
 
-function shuffle() {
-for (i=0; i < 1000; i++);
+function shuffleDeck() {
+  for(i = 0; i < 52; i++) {
 
+  }
 }
