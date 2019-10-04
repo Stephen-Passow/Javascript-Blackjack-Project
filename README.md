48eff66157e2e299cb4a4ae8cd64b5b33ad9a366
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-10-04 10:02:41 -0400

    finished css/html buttons

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# blackjack-project
+# blackjack game-project 1
+For project 1 I made a Blackjack game with just 1 person versus the dealer. 
+---------------Below are the rules of the game.---------------
+
+The player attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.
+
+ At the start of the game, the dealer gives one card face up to the player and then onecard face up to themselves. Another round of cards is then dealt face up to the player, but the dealer takes the second card face down. 
+If the player's first two cards are an ace and a "ten-card" (a King Queen or Jack), giving a count of 21 in two cards, this is a natural or "blackjack." If the dealer has a natural, they automatically win the hand.
+
+The player goes and must decide whether to "stand" (not ask for another card) or "hit" (ask for another card in an attempt to get closer to a count of 21, or even hit 21 exactly). Thus, the player may stand on the two cards originally dealt to them, or they may ask the dealer for additional cards, one at a time, until deciding to stand on the total (if it is 21 or under), or goes "bust" (if it is over 21) and the player loses. 
+
+
+When the dealer has served, the dealers face-down card is turned up. If the total is 17 or more, it must stand. If the total is 16 or under, they must take a card. The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand. The dealer's decisions, then, are automatic on all plays, whereas the player always has the option of taking one or more cards.
+
 ![Wire Frame](css/wireframe.jpg)
---
+++ script.js
@@ -0,0 +1,93 @@
+// //card suits and values
+// let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+// let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+// const Deck = {
+//   constructor() {
+//     let deck = [];
+
+//     const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+//     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+//     for (let i=0; i < suits.length; i++) {
+//       for (let x=0; x < values.length; x++) {
+//         deck.push(`${values} of ${suits}`);
+//       }
+//     }
+//     return deck;
+//   }
+// }
+// console.log(Deck.constructor)
+
+// const Deck = {
+//     createDeck () {
+//       let deck = [];
+
+//     const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+//     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+//       for (let i=0; i < suits.length; i++) {
+//       for (let x=0; x < values.length; x++) {
+//         deck.push(`${values} of ${suits}`);
+//   }
+//     }
+//     console.log()
+//   }
+// }
+const deckArray = [
+    {card: 'hearts2' , image: '2_of_hearts.png'},
+    {card: 'hearts3' , image: '3_of_hearts.png'},
+    {card: 'hearts4' , image: '4_of_hearts.png'},
+    {card: 'hearts5' , image: '5_of_hearts.png'},
+    {card: 'hearts6' , image: '6_of_hearts.png'},
+    {card: 'hearts7' , image: '7_of_hearts.png'},
+    {card: 'hearts8' , image: '8_of_hearts.png'},
+    {card: 'hearts9' , image: '9_of_hearts.png'},
+    {card: 'heartsJ' , image: 'J_of_hearts.png'},
+    {card: 'heartsQ' , image: 'Q_of_hearts.png'},
+    {card: 'heartsK' , image: 'K_of_hearts.png'},
+    {card: 'heartsA' , image: 'A_of_hearts.png'},
+
+    {card: 'spades2' , image: '2_of_spades.png'},
+    {card: 'spades3' , image: '3_of_spades.png'},
+    {card: 'spades4' , image: '4_of_spades.png'},
+    {card: 'spades5' , image: '5_of_spades.png'},
+    {card: 'spades6' , image: '6_of_spades.png'},
+    {card: 'spades7' , image: '7_of_spades.png'},
+    {card: 'spades8' , image: '8_of_spades.png'},
+    {card: 'spades9' , image: '9_of_spades.png'},
+    {card: 'spadesJ' , image: 'J_of_spades.png'},
+    {card: 'spadesQ' , image: 'Q_of_spades.png'},
+    {card: 'spadesK' , image: 'K_of_spades.png'},
+    {card: 'spadesA' , image: 'A_of_spades.png'},
+
+    {card: 'clubs2' , image: '2_of_clubs.png'},
+    {card: 'clubs3' , image: '3_of_clubs.png'},
+    {card: 'clubs4' , image: '4_of_clubs.png'},
+    {card: 'clubs5' , image: '5_of_clubs.png'},
+    {card: 'clubs6' , image: '6_of_clubs.png'},
+    {card: 'clubs7' , image: '7_of_clubs.png'},
+    {card: 'clubs8' , image: '8_of_clubs.png'},
+    {card: 'clubs9' , image: '9_of_clubs.png'},
+    {card: 'clubsJ' , image: 'J_of_clubs.png'},
+    {card: 'clubsQ' , image: 'Q_of_clubs.png'},
+    {card: 'clubsK' , image: 'K_of_clubs.png'},
+    {card: 'clubsA' , image: 'A_of_clubs.png'},
+
+    {card: 'diamonds2' , image: '2_of_clubs.png'},
+    {card: 'diamonds3' , image: '3_of_clubs.png'},
+    {card: 'diamonds4' , image: '4_of_clubs.png'},
+    {card: 'diamonds5' , image: '5_of_clubs.png'},
+    {card: 'diamonds6' , image: '6_of_clubs.png'},
+    {card: 'diamonds7' , image: '7_of_clubs.png'},
+    {card: 'diamonds8' , image: '8_of_clubs.png'},
+    {card: 'diamonds9' , image: '9_of_clubs.png'},
+    {card: 'diamondsJ' , image: 'J_of_clubs.png'},
+    {card: 'diamondsQ' , image: 'Q_of_clubs.png'},
+    {card: 'diamondsK' , image: 'K_of_clubs.png'},
+    {card: 'diamondsA' , image: 'A_of_clubs.png'},
+
+    
+  ]
+    
+  
